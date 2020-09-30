4a9caa2b95b469656383f317f88258cc9cdd1647
Ben Walandow <bentol777@yahoo.com>
2020-09-30 13:04:57 +0700

    save

--- resources/views/component/contact.blade.php
+++ resources/views/component/contact.blade.php
@@ -1,7 +1,7 @@
 <!-- Begin of contact -->
 <section data-id="contact" class="section section-page fp-auto-height-responsive section-contact {{Request::segment(1)=='contact'?'top-margin':''}}">
     <div class="cover-bg pos-abs size-full bg-img w-100 h-100"
-         data-image-src="{{el_url($contact,'contact-background')}}"
+         {{--data-image-src="{{el_url($contact,'contact-background')}}"--}}
          style="background-repeat: no-repeat;
                  background-position: center center;
                  background-size: cover;"></div>
