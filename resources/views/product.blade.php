fd785a5550cd4b3e813faca8fafc3783c6dc92bd
Ben Walandow <bentol777@yahoo.com>
2020-12-08 18:59:44 +0700

    tags

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -151,7 +151,7 @@ $body = $title.', '.$link;
                                         </div>
                                         <div class="item-content">
                                             <p style="font-size: 12pt; min-height:80px">
-                                                {{strip_tags($produk->meta_description)}}
+                                                {{strip_tags($row->deskripsi)}}
                                             </p>
                                             <a class="btn btn-line-a mt4" href="{{route('product',['slug'=>$row->slug])}}">
                                                 <span class="text">Lihat detail</span>
